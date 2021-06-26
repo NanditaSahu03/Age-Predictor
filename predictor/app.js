d582aabb163df0ef7c2726f7f500441ed4b899ee
nandita sahu <Savegirlchild@users.noreply.github.com>
2021-06-26 23:26:41 +0530

    Create app.js

---
+++ predictor/app.js
@@ -0,0 +1,31 @@
+//Initalize Age
+const age = new Age;
+
+//Initialize UI
+const ui = new UI;
+
+//Search input
+const username = document.getElementById('searchName');
+
+//Add event listener to name
+username.addEventListener('keyup',(e) => {
+   const nameText=e.target.value;
+
+   if (nameText !== '') {
+     //Make http call
+     age.getAge(nameText)
+     .then(data => {
+      //  if (data.age === null) {
+      //    //Show alert
+      //    ui.showAlert('Give a correct name','alert alert-danger')
+
+         //Show profile
+         //console.log(data.name);
+         ui.showProfile(data);
+     })
+     
+   }else{
+     //Clear Fields
+     ui.clearFields();
+   }
+});

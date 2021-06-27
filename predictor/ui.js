677835200c19dae43df1a0eee2a61bd121d122b5
nandita sahu <Savegirlchild@users.noreply.github.com>
2021-06-27 21:28:22 +0530

    Update ui.js

--- predictor/ui.js
+++ predictor/ui.js
@@ -5,50 +5,54 @@ class UI{
 
  //Show profile
   showProfile(data){
-    console.log(data.age);
-    this.profile.innerHTML = 
-    `<div>
-    <span>User Name: ${data.name}</span>
-    <span>User Age: ${data.age}</span>
-    <span>User Count: ${data.count}</span>
+    //console.log(data.age);
+    //console.log(data.name);
+    profile.innerHTML = 
+    `<div class="col-md-9">
+    <div  style="margin-bottom: 5px;background-color	:rgb(69, 130, 236);width:135%;text-align: center;color:white;">User Name: ${data.name}</div>
+    <br>
+    <div style="margin-bottom: 5px;background-color	:grey; text-align: center;color:white;width:135%">User Age: ${data.age}</div>
+    <br>
+    <div style="margin-bottom: 5px;background-color	:rgb(69, 130, 236); text-align: center;color:white;width:135%">User Count: ${data.count}</div>
     </div>`
     ;
   }
 
-  // //Show alert
-  // showAlert(message,className){
-  //   // Clear any remaining alerts
-  //   this.clearAlert();
-  //   // Create div
-  //   const div  =  document.createElement('div');
-  //   // Add classes
-  //   div.className = className;
-  //   // Add text
-  //   div.appendChild(document.createTextNode(message));
-  //   // Get parent
-  //   const container =  document.querySelector('.searchContainer');
-  //   // Get search box
-  //   const search = document.querySelector('.search');
-  //   // Insert alert
-  //   container.insertBefore(div, search);
+  //Show alert
+  showAlert(message,className){
+    // Clear any remaining alerts
+    this.clearAlert();
+    // Create div
+    const div  =  document.createElement('div');
+    // Add classes
+    div.className = className;
+    // Add text
+    div.appendChild(document.createTextNode(message));
+    // Get parent
+    const container =  document.querySelector('.searchContainer');
+    // Get search box
+    const search = document.querySelector('.search');
+    // Insert alert
+    container.insertBefore(div, search);
 
-  //   // Timeout after 3 sec
-  //   setTimeout(() => {
-  //     this.clearAlert();
-  //   }, 3000);
-  // }
 
-  // //Clear Alert
-  // clearAlert() {
-  //   const currentAlert = document.querySelector('.alert');
+    // Timeout after 3 sec
+    setTimeout(() => {
+      this.clearAlert();
+    }, 3000);
+  }
 
-  //   if(currentAlert){
-  //     currentAlert.remove();
-  //   }
-  // }
+  //Clear Alert
+  clearAlert() {
+    const currentAlert = document.querySelector('.alert');
+
+    if(currentAlert){
+      currentAlert.remove();
+    }
+  }
 
   //Clear Fields
   clearFields(){
-    this.profile.innerHtml='';
+    profile.innerHtml='';
   }
 }
